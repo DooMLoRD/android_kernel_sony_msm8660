55418fa7ba15868c716ff2dbafe191563536b950
DooMLoRD <metoo.mememe@gmail.com>
2013-06-01 17:18:29 +0530

    merged 6.2.B.0.200 firmware sources

---
+++ AndroidKernel.mk
@@ -0,0 +1,121 @@
+#Android makefile to build kernel as a part of Android Build
+PERL		= perl
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+
+KERNEL_OUT := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ
+KERNEL_CONFIG := $(KERNEL_OUT)/.config
+TARGET_PREBUILT_INT_KERNEL := $(KERNEL_OUT)/arch/arm/boot/zImage
+KERNEL_HEADERS_INSTALL := $(KERNEL_OUT)/usr
+KERNEL_MODULES_INSTALL := system
+KERNEL_MODULES_OUT := $(TARGET_OUT)/lib/modules
+KERNEL_IMG=$(KERNEL_OUT)/arch/arm/boot/Image
+
+MSM_ARCH ?= $(shell $(PERL) -e 'while (<>) {$$a = $$1 if /CONFIG_ARCH_((?:MSM|QSD)[a-zA-Z0-9]+)=y/; $$r = $$1 if /CONFIG_MSM_SOC_REV_(?!NONE)(\w+)=y/;} print lc("$$a$$r\n");' $(KERNEL_CONFIG))
+KERNEL_USE_OF ?= $(shell $(PERL) -e '$$of = "n"; while (<>) { if (/CONFIG_USE_OF=y/) { $$of = "y"; break; } } print $$of;' kernel/arch/arm/configs/$(KERNEL_DEFCONFIG))
+
+ifeq "$(KERNEL_USE_OF)" "y"
+DTS_NAME ?= $(MSM_ARCH)
+DTS_FILES = $(wildcard $(TOP)/kernel/arch/arm/boot/dts/$(DTS_NAME)*.dts)
+DTS_FILE = $(lastword $(subst /, ,$(1)))
+DTB_FILE = $(addprefix $(KERNEL_OUT)/arch/arm/boot/,$(patsubst %.dts,%.dtb,$(call DTS_FILE,$(1))))
+ZIMG_FILE = $(addprefix $(KERNEL_OUT)/arch/arm/boot/,$(patsubst %.dts,%-zImage,$(call DTS_FILE,$(1))))
+KERNEL_ZIMG = $(KERNEL_OUT)/arch/arm/boot/zImage
+DTC = $(KERNEL_OUT)/scripts/dtc/dtc
+
+define append-dtb
+mkdir -p $(KERNEL_OUT)/arch/arm/boot;\
+$(foreach d, $(DTS_FILES), \
+   $(DTC) -p 1024 -O dtb -o $(call DTB_FILE,$(d)) $(d); \
+   cat $(KERNEL_ZIMG) $(call DTB_FILE,$(d)) > $(call ZIMG_FILE,$(d));)
+endef
+else
+
+define append-dtb
+endef
+endif
+
+ifeq ($(TARGET_USES_UNCOMPRESSED_KERNEL),true)
+$(info Using uncompressed kernel)
+TARGET_PREBUILT_KERNEL := $(KERNEL_OUT)/piggy
+else
+TARGET_PREBUILT_KERNEL := $(TARGET_PREBUILT_INT_KERNEL)
+endif
+
+define mv-modules
+mdpath=`find $(KERNEL_MODULES_OUT) -type f -name modules.dep`;\
+if [ "$$mdpath" != "" ];then\
+mpath=`dirname $$mdpath`;\
+ko=`find $$mpath/kernel -type f -name *.ko`;\
+for i in $$ko; do mv $$i $(KERNEL_MODULES_OUT)/; done;\
+fi
+endef
+
+define clean-module-folder
+mdpath=`find $(KERNEL_MODULES_OUT) -type f -name modules.dep`;\
+if [ "$$mdpath" != "" ];then\
+mpath=`dirname $$mdpath`; rm -rf $$mpath;\
+fi
+endef
+
+$(KERNEL_OUT):
+	mkdir -p $(KERNEL_OUT)
+
+$(KERNEL_CONFIG): $(KERNEL_OUT)
+	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi- $(KERNEL_DEFCONFIG)
+
+$(KERNEL_OUT)/piggy : $(TARGET_PREBUILT_INT_KERNEL)
+	$(hide) gunzip -c $(KERNEL_OUT)/arch/arm/boot/compressed/piggy.gzip > $(KERNEL_OUT)/piggy
+
+$(TARGET_PREBUILT_INT_KERNEL): $(KERNEL_OUT) $(KERNEL_CONFIG) $(KERNEL_HEADERS_INSTALL)
+	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi-
+	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi- modules
+	$(MAKE) -C kernel O=../$(KERNEL_OUT) INSTALL_MOD_PATH=../../$(KERNEL_MODULES_INSTALL) ARCH=arm CROSS_COMPILE=arm-eabi- modules_install
+	$(mv-modules)
+	$(clean-module-folder)
+	$(append-dtb)
+
+$(KERNEL_HEADERS_INSTALL): $(KERNEL_OUT) $(KERNEL_CONFIG)
+	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi- headers_install
+
+kerneltags: $(KERNEL_OUT) $(KERNEL_CONFIG)
+	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi- tags
+
+kernelconfig: $(KERNEL_OUT) $(KERNEL_CONFIG)
+	env KCONFIG_NOTIMESTAMP=true \
+	     $(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi- menuconfig
+	env KCONFIG_NOTIMESTAMP=true \
+	     $(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi- savedefconfig
+	cp $(KERNEL_OUT)/defconfig kernel/arch/arm/configs/$(KERNEL_DEFCONFIG)
+endif
+
+#
+# Rules for packing kernel into elf and sin
+#
+$(PRODUCT_OUT)/cmdline.txt: device/semc/$(TARGET_PRODUCT)/BoardConfig.mk
+	$(hide) echo -n '$(BOARD_KERNEL_CMDLINE)' > $@
+
+$(PRODUCT_OUT)/kernel-unsigned.elf: $(TARGET_PREBUILT_KERNEL) $(PRODUCT_OUT)/ramdisk.img $(PRODUCT_OUT)/RPM.bin $(PRODUCT_OUT)/cmdline.txt | sin-tools
+	$(hide) $(HOST_OUT_EXECUTABLES)/mkelf.py -o $@ \
+		$(TARGET_PREBUILT_KERNEL)@$(BOARD_KERNEL_ADDR) \
+		$(PRODUCT_OUT)/ramdisk.img@$(BOARD_RAMDISK_ADDR),ramdisk \
+		$(PRODUCT_OUT)/RPM.bin@$(BOARD_RPM_ADDR),rpm
+
+$(PRODUCT_OUT)/kernel-signed.elf: $(PRODUCT_OUT)/kernel-unsigned.elf $(PRODUCT_PARTITION_CONFIG) | sin-tools
+	$(hide) $(SEMCSC) -c $(PRODUCT_PARTITION_CONFIG) -p Kernel -t internal -i $< -o $@
+
+$(PRODUCT_OUT)/kernel.si_: $(PRODUCT_OUT)/kernel-signed.elf | sin-tools
+	$(hide) $(HOST_OUT_EXECUTABLES)/create_sin_header Kernel $(PRODUCT_PARTITION_CONFIG) $@
+	$(hide) cat $< >> $@
+
+$(PRODUCT_OUT)/kernel.sin: $(PRODUCT_OUT)/kernel.si_ $(PRODUCT_PARTITION_CONFIG) | sin-tools
+	@echo target SIN: $(notdir $@)
+	$(hide) $(SEMCSC) -c $(PRODUCT_PARTITION_CONFIG) -p Kernel -t external -i $< -o $@
+
+#
+# Add kernel to system wide PHONY target sin
+#
+.PHONY: sin
+
+sin: $(PRODUCT_OUT)/kernel.sin
+
---
+++ drivers/staging/android/lowmemorykiller.c
@@ -0,0 +1,315 @@
+/* drivers/misc/lowmemorykiller.c
+ *
+ * The lowmemorykiller driver lets user-space specify a set of memory thresholds
+ * where processes with a range of oom_score_adj values will get killed. Specify
+ * the minimum oom_score_adj values in
+ * /sys/module/lowmemorykiller/parameters/adj and the number of free pages in
+ * /sys/module/lowmemorykiller/parameters/minfree. Both files take a comma
+ * separated list of numbers in ascending order.
+ *
+ * For example, write "0,8" to /sys/module/lowmemorykiller/parameters/adj and
+ * "1024,4096" to /sys/module/lowmemorykiller/parameters/minfree to kill
+ * processes with a oom_score_adj value of 8 or higher when the free memory
+ * drops below 4096 pages and kill processes with a oom_score_adj value of 0 or
+ * higher when the free memory drops below 1024 pages.
+ *
+ * The driver considers memory used for caches to be free, but if a large
+ * percentage of the cached memory is locked this can be very inaccurate
+ * and processes may not get killed until the normal oom killer is triggered.
+ *
+ * Copyright (C) 2007-2008 Google, Inc.
+ * Copyright (C) 2012-2013 Sony Mobile Communications AB.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/mm.h>
+#include <linux/oom.h>
+#include <linux/sched.h>
+#include <linux/rcupdate.h>
+#include <linux/notifier.h>
+#include <linux/slab.h>
+#include <linux/ktime.h>
+#include <linux/spinlock.h>
+
+static uint32_t lowmem_debug_level = 2;
+static int lowmem_adj[6] = {
+	0,
+	1,
+	6,
+	12,
+};
+static int lowmem_adj_size = 4;
+static int lowmem_minfree[6] = {
+	3 * 512,	/* 6MB */
+	2 * 1024,	/* 8MB */
+	4 * 1024,	/* 16MB */
+	16 * 1024,	/* 64MB */
+};
+static int lowmem_minfree_size = 4;
+
+static ktime_t lowmem_deathpending_timeout;
+
+static LIST_HEAD(ignorelist_head);
+struct killed_info {
+	int marked;
+	unsigned long jiffies;
+	char comm[TASK_COMM_LEN];
+	struct list_head list;
+};
+static struct killed_info *ignorelist_mempool;
+static unsigned int ignore_timeout_sec = 60;
+static DEFINE_SPINLOCK(lock_ignorelist);
+#define MAX_IGNORELIST 100
+#define LMK_BUSY (-1)
+
+#define lowmem_print(level, x...)			\
+	do {						\
+		if (lowmem_debug_level >= (level))	\
+			printk(x);			\
+	} while (0)
+
+static int is_recently_selected(char *comm)
+{
+	int ret = 0;
+	int listnum = 0;
+	struct list_head *p, *n;
+
+	spin_lock(&lock_ignorelist);
+	list_for_each_safe(p, n, &ignorelist_head) {
+		struct killed_info *ki;
+		unsigned long timeout;
+		ki = list_entry(p, struct killed_info, list);
+		timeout = ki->jiffies + ignore_timeout_sec * HZ;
+		if (time_after(jiffies, timeout)) {
+			list_del(&ki->list);
+			ki->marked = 0;
+		} else if ((strncmp(comm, ki->comm, TASK_COMM_LEN) == 0)) {
+			lowmem_print(1, "lowmem skipped %s\n", ki->comm);
+			ret = 1;
+		}
+		listnum++;
+	}
+	lowmem_print(4, "ignore list num %d\n", listnum);
+	spin_unlock(&lock_ignorelist);
+	return ret;
+}
+
+static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
+{
+	static DEFINE_SPINLOCK(lowmem_lock);
+	struct task_struct *tsk;
+	struct task_struct *selected = NULL;
+	int rem = 0;
+	static int same_count;
+	static int busy_count;
+	static int busy_count_dropped;
+	static int oldpid;
+	static int lastpid;
+	static ktime_t next_busy_print;
+	int tasksize;
+	int i;
+	int min_score_adj = OOM_SCORE_ADJ_MAX + 1;
+	int selected_tasksize = 0;
+	int selected_oom_score_adj;
+	int array_size = ARRAY_SIZE(lowmem_adj);
+	int other_free = global_page_state(NR_FREE_PAGES);
+	int other_file = global_page_state(NR_FILE_PAGES) -
+						global_page_state(NR_SHMEM);
+
+	if (lowmem_adj_size < array_size)
+		array_size = lowmem_adj_size;
+	if (lowmem_minfree_size < array_size)
+		array_size = lowmem_minfree_size;
+	for (i = 0; i < array_size; i++) {
+		if (other_free < lowmem_minfree[i] &&
+		    other_file < lowmem_minfree[i]) {
+			min_score_adj = lowmem_adj[i];
+			break;
+		}
+	}
+	if (sc->nr_to_scan > 0)
+		lowmem_print(3, "lowmem_shrink %lu, %x, ofree %d %d, ma %d\n",
+				sc->nr_to_scan, sc->gfp_mask, other_free,
+				other_file, min_score_adj);
+	rem = global_page_state(NR_ACTIVE_ANON) +
+		global_page_state(NR_ACTIVE_FILE) +
+		global_page_state(NR_INACTIVE_ANON) +
+		global_page_state(NR_INACTIVE_FILE);
+	if (sc->nr_to_scan <= 0 || min_score_adj == OOM_SCORE_ADJ_MAX + 1) {
+		lowmem_print(5, "lowmem_shrink %lu, %x, return %d\n",
+			     sc->nr_to_scan, sc->gfp_mask, rem);
+		return rem;
+	}
+	selected_oom_score_adj = min_score_adj;
+
+	if (spin_trylock(&lowmem_lock) == 0) {
+		if (ktime_us_delta(ktime_get(), next_busy_print) > 0) {
+			lowmem_print(3, "Lowmemkiller busy %d %d %d\n",
+				busy_count, busy_count_dropped,
+				oom_killer_disabled);
+			next_busy_print = ktime_add(ktime_get(),
+						    ktime_set(5, 0));
+			busy_count_dropped = 0;
+		}
+		busy_count++;
+		busy_count_dropped++;
+		return LMK_BUSY;
+	}
+	/* turn of scheduling to protect task list */
+	rcu_read_lock();
+	for_each_process(tsk) {
+		struct task_struct *p;
+		int oom_score_adj;
+
+		if (tsk->flags & PF_KTHREAD)
+			continue;
+
+		p = find_lock_task_mm(tsk);
+		if (!p)
+			continue;
+
+		if (test_tsk_thread_flag(p, TIF_MEMDIE) &&
+			ktime_us_delta(ktime_get(),
+				lowmem_deathpending_timeout) < 0) {
+			task_unlock(p);
+			same_count++;
+			if (p->pid != oldpid || same_count > 1000) {
+				lowmem_print(3,
+					"terminate %d (%s) old:%d last:%d %ld %d\n",
+					p->pid, p->comm, oldpid, lastpid,
+					(long)ktime_us_delta(ktime_get(),
+						lowmem_deathpending_timeout),
+					same_count);
+				lowmem_print(3,
+					"state:%ld flag:0x%x busy:%d %d\n",
+					p->state, p->flags,
+					busy_count, oom_killer_disabled);
+				oldpid = p->pid;
+				same_count = 0;
+			}
+			rcu_read_unlock();
+			spin_unlock(&lowmem_lock);
+			/* wait one jiffie */
+			schedule_timeout(1);
+			return LMK_BUSY;
+		}
+		oom_score_adj = p->signal->oom_score_adj;
+		if (oom_score_adj < min_score_adj) {
+			task_unlock(p);
+			continue;
+		}
+		tasksize = get_mm_rss(p->mm);
+		task_unlock(p);
+
+		/* Check recently killed list */
+		if (is_recently_selected(p->comm))
+			continue;
+
+		if (tasksize <= 0)
+			continue;
+		if (selected) {
+			if (oom_score_adj < selected_oom_score_adj)
+				continue;
+			if (oom_score_adj == selected_oom_score_adj &&
+			    tasksize <= selected_tasksize)
+				continue;
+		}
+		selected = p;
+		selected_tasksize = tasksize;
+		selected_oom_score_adj = oom_score_adj;
+		lowmem_print(4, "select %d (%s), adj %d, size %d, to kill\n",
+			     p->pid, p->comm, oom_score_adj, tasksize);
+	}
+	if (selected) {
+		/* Add recently killed list */
+		int i;
+
+		spin_lock(&lock_ignorelist);
+		for (i = 0; i < MAX_IGNORELIST; i++) {
+			struct killed_info *ki;
+			ki = ignorelist_mempool + i;
+			if (ki->marked == 0) {
+				strlcpy(ki->comm, selected->comm,
+					TASK_COMM_LEN);
+				ki->jiffies = jiffies;
+				ki->marked = 1;
+				list_add(&ki->list, &ignorelist_head);
+				break;
+			}
+		}
+		spin_unlock(&lock_ignorelist);
+
+		lowmem_print(1, "send sigkill to %d (%s), adj %d, size %d\n",
+			     selected->pid, selected->comm,
+			     selected_oom_score_adj, selected_tasksize);
+		send_sig(SIGKILL, selected, 0);
+
+		lowmem_deathpending_timeout = ktime_add_ns(ktime_get(),
+							   NSEC_PER_SEC/2);
+		lowmem_print(2, "state:%ld flag:0x%x busy:%d %d\n",
+			     selected->state, selected->flags,
+			     busy_count, oom_killer_disabled);
+		lastpid = selected->pid;
+		set_tsk_thread_flag(selected, TIF_MEMDIE);
+		rem -= selected_tasksize;
+	}
+	lowmem_print(4, "lowmem_shrink %lu, %x, return %d\n",
+		     sc->nr_to_scan, sc->gfp_mask, rem);
+	rcu_read_unlock();
+	spin_unlock(&lowmem_lock);
+	return rem;
+}
+
+static struct shrinker lowmem_shrinker = {
+	.shrink = lowmem_shrink,
+	.seeks = DEFAULT_SEEKS * 16
+};
+
+static int __init lowmem_init(void)
+{
+	ignorelist_mempool = (struct killed_info *)
+		kzalloc(sizeof(struct killed_info) * MAX_IGNORELIST,
+			GFP_KERNEL);
+	if (ignorelist_mempool == NULL)
+		return -ENOMEM;
+	register_shrinker(&lowmem_shrinker);
+	return 0;
+}
+
+static void __exit lowmem_exit(void)
+{
+	while (!list_empty(&ignorelist_head)) {
+		struct killed_info *ki;
+		ki = list_entry(ignorelist_head.next,
+			struct killed_info, list);
+		list_del(&ki->list);
+	}
+	kfree(ignorelist_mempool);
+	unregister_shrinker(&lowmem_shrinker);
+}
+
+module_param_named(cost, lowmem_shrinker.seeks, int, S_IRUGO | S_IWUSR);
+module_param_array_named(adj, lowmem_adj, int, &lowmem_adj_size,
+			 S_IRUGO | S_IWUSR);
+module_param_array_named(minfree, lowmem_minfree, uint, &lowmem_minfree_size,
+			 S_IRUGO | S_IWUSR);
+module_param_named(debug_level, lowmem_debug_level, uint, S_IRUGO | S_IWUSR);
+module_param_named(ignore_timeout_sec, ignore_timeout_sec, uint,
+			S_IRUGO | S_IWUSR);
+
+module_init(lowmem_init);
+module_exit(lowmem_exit);
+
+MODULE_LICENSE("GPL");
+
